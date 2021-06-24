80e879b751efc652bf32b5f6b75c9c9cb3379299
voelkely <48826808+voelkely@users.noreply.github.com>
2021-06-24 16:26:03 +0200

    Update main.js

--- Konzeption/Client/main.js
+++ Konzeption/Client/main.js
@@ -40,10 +40,6 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 };
-	
-function myFunction() {
-    window.open("https://www.w3schools.com");
-}
 
 function getQuestion() {
     console.log("get Question"); 
